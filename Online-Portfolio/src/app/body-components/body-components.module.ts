7c7cbb51d022c6c9f47644459a2a0ec1954b2158
Orla <orlaith.coleman@gmail.com>
2019-11-15 14:37:41 +1100

    add components and node modules

---
+++ Online-Portfolio/src/app/body-components/body-components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [about],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    
+  ]
+})
+export class BodyComponentsModule { }
---
+++ Online-Portfolio/src/app/body/body.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainBodyComponent } from './main-body/main-body.component';
+
+@NgModule({
+  declarations: [MainBodyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BodyModule { }
