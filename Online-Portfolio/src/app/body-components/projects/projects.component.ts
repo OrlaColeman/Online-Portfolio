6f275931d822488f4112ddc7e9d14ac8599d8187
Orla <orlaith.coleman@gmail.com>
2019-11-19 15:07:58 +1100

    add cover for project vids

--- Online-Portfolio/src/app/body-components/projects/projects.component.ts
+++ Online-Portfolio/src/app/body-components/projects/projects.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,29 +9,52 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 code = "<Code>"
 switchBtn: String = "more";
+rachel_vid: any;
+rachel_cover: any;
+tempo_vid: any;
+tempo_cover: any;
   constructor() { }
 
   ngOnInit() {
-    document.getElementById('other-projects').style.display='none';
-    document.getElementById('snapshots').style.display='none';
-    document.getElementById('info-card').style.display='none';
-  }
-  showInfoBox(){
-    document.getElementById('info-card').style.display='block';
-    document.getElementById('snapshots').style.display='none';
-    document.getElementById('code').style.display='none';
-  }
-  showSnapShots(){
-    document.getElementById('info-card').style.display='none';
-    document.getElementById('snapshots').style.display='block';
-    document.getElementById('code').style.display='none';
-  }
-  showCode(){
-    document.getElementById('info-card').style.display='none';
-    document.getElementById('snapshots').style.display='none';
-    document.getElementById('code').style.display='block';
+    this.rachel_cover = document.getElementById('rachel_cover');
+    this.rachel_vid = document.getElementById('styled_by_rachel_vid');
+    this.rachel_vid.style.display='none';
+    this.tempo_cover = document.getElementById('tempo_cover');
+    this.tempo_vid = document.getElementById('tempo_antiques_vid');
+    this.tempo_vid.style.display='none';
 
+    // setTimeout(function () {
+    //   this.changeState();
+    // }.bind(this), 800);
+    // document.getElementById('other-projects').style.display='none';
+    // document.getElementById('snapshots').style.display='none';
+    // document.getElementById('info-card').style.display='none';
   }
+
+  showRachelVid(){
+this.rachel_cover.style.display='none';
+this.rachel_vid.style.display='block';
+  }
+  showTempoVid(){
+this.tempo_cover.style.display='none';
+this.tempo_vid.style.display='block';
+  }
+  // showInfoBox(){
+  //   document.getElementById('info-card').style.display='block';
+  //   document.getElementById('snapshots').style.display='none';
+  //   document.getElementById('code').style.display='none';
+  // }
+  // showSnapShots(){
+  //   document.getElementById('info-card').style.display='none';
+  //   document.getElementById('snapshots').style.display='block';
+  //   document.getElementById('code').style.display='none';
+  // }
+  // showCode(){
+  //   document.getElementById('info-card').style.display='none';
+  //   document.getElementById('snapshots').style.display='none';
+  //   document.getElementById('code').style.display='block';
+
+  // }
   showMore(e){
    console.log(e)
    if(e === "more"){
