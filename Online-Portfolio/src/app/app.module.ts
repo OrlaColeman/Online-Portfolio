51f457c1418e03a42e2922f9ba4599491f036ffb
Orla <orlaith.coleman@gmail.com>
2019-11-20 16:37:11 +1100

    add loader

--- Online-Portfolio/src/app/app.module.ts
+++ Online-Portfolio/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { BodyComponentsModule } from './body-components/body-components.module';
 import { BodyModule } from './body/body.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { LoaderModule } from './loader/loader.module';
+import { LoadingScreenComponent } from './loader/loading-screen/loading-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BodyComponentsModule,
     BodyModule,
+    LoaderModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
