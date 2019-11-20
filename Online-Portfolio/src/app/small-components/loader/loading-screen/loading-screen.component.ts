a0d1c2643c932add9101a29d5b57fd9936b320ab
Orla <orlaith.coleman@gmail.com>
2019-11-20 16:29:59 +1100

    add loader to app

---
+++ Online-Portfolio/src/app/small-components/loader/loading-screen/loading-screen.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoadingScreenService } from '../loading-screen.service';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-loading-screen',
+  templateUrl: './loading-screen.component.html',
+  styleUrls: ['./loading-screen.component.css']
+})
+export class LoadingScreenComponent implements OnInit, OnDestroy {
+
+  loading: boolean = false;
+  loadingSubscription: Subscription;
+
+  constructor(private loadingScreenService: LoadingScreenService) { }
+
+  ngOnInit() {
+    this.loadingSubscription = this.loadingScreenService.loadingStatus
+     .pipe(debounceTime(200))
+    .subscribe((value) => {
+      this.loading = value;
+    });
+  }
+  ngOnDestroy(){
+    this.loadingSubscription.unsubscribe();
+  }
+
+}
